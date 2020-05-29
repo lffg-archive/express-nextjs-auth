29e3500f1d50b12bae879b4814d7570dcdc20af4
Luiz Felipe Gonçalves <23662020+lffg@users.noreply.github.com>
2020-05-29 17:35:01 -0300

    server: Use predefined message

--- packages/server/src/middlewares/auth.ts
+++ packages/server/src/middlewares/auth.ts
@@ -1,15 +1,13 @@
 import { Handler } from 'express';
+import { http } from '../utils';
 
 export const auth: Handler = (req, res, next) => {
   const userData = req.session?.userData;
 
   if (!userData) {
-    return res.status(401).json({
-      error: {
-        message: 'Forbidden unauthenticated access.',
-        code: 'ERR_UNAUTHENTICATED'
-      }
-    });
+    return res
+      .status(401)
+      .json(http.err('Unauthorized access.', 'ERR_UNAUTHORIZED'));
   }
 
   next();
